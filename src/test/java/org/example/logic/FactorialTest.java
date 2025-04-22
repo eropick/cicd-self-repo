bfe587eeb6ff5cc665c090594aef4e29ed719105
eropick <34877404+eropick@users.noreply.github.com>
2025-04-22 20:42:30 +0900

    test Failed Case

--- src/test/java/org/example/logic/FactorialTest.java
+++ src/test/java/org/example/logic/FactorialTest.java
@@ -26,7 +26,7 @@ class FactorialTest {
     @Test
     @DisplayName("음수 입력")
     public void testInputNegativeNum(){
-        Factorial fact = new Factorial(-1);
+        Factorial fact = new Factorial(3);
         assertTrue(fact.getResult().isEmpty()); //nullable => empty가 반환됨
     }
 
